72cf5139ed3fa220099ec2b71ded36a3a83de728
Dwn96 <45044744+Dwn96@users.noreply.github.com>
2020-03-20 13:21:39 +0300

    Updated routes

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 //@desc Get all bootcamps
 //@route GET /api/v1/bootcamps
 //access  public -you dont need a token(no authentication)
@@ -17,8 +19,13 @@ exports.getBootcamp = (req, res, next) => {
 //@desc Create a  bootcamp
 //@route POST /api/v1/bootcamps
 //access  Private -you have to send a token(need to be logged in)
-exports.createBootcamp = (req, res, next) => {
-  res.status(400).json({ success: true, message: 'Create new bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@desc Update bootcamp
--- server.js
+++ server.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+//load env variables
+dotenv.config({ path: './config/config.env' });
+
+//Connect to database
+connectDB();
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
-//load config file
-dotenv.config({ path: './config/config.env' });
-
 const app = express();
+
+//Body parser
+app.use(express.json());
+
+//Dev logging middleware
+//runs only if we're in development environmnent
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
   )
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server and exit
+  server.close(() => process.exit(1));
+});
